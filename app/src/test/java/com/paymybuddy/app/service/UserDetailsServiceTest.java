9ad2035e468fbf51729ff43da0b13bd69b9eedca
Quentin Caracatzanis <quentinversaille@gmail.com>
2022-11-14 14:15:11 +0100

    Test of UserDetailsServiceImpl
    implementation of the Test of UserDetailsServiceImpl

---
+++ app/src/test/java/com/paymybuddy/app/service/UserDetailsServiceTest.java
@@ -0,0 +1,55 @@
+package com.paymybuddy.app.service;
+
+import com.paymybuddy.app.model.User;
+import com.paymybuddy.app.repository.UserRepository;
+import com.paymybuddy.app.service.implementation.UserDetailsServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+public class UserDetailsServiceTest {
+
+    @InjectMocks
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Test
+    public void testGivenAUserEmailThenReturnUserDetails() throws Exception {
+        User user = new User();
+        user.setPassword("test");
+        user.setAddress("test");
+        user.setEmail("test");
+        user.setBirthdate(new Date(10/10/2000));
+        user.setFirstName("test");
+        user.setLastName("test");
+
+        when(userRepository.findByEmail(anyString())).thenReturn(user);
+        UserDetails result = userDetailsService.loadUserByUsername("test");
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testGivenAnEmailNonExistantShouldReturnExceptions() throws Exception {
+        when(userRepository.findByEmail(anyString())).thenReturn(null);
+        Exception exception = assertThrows(UsernameNotFoundException.class,
+                () -> userDetailsService.loadUserByUsername(""));
+    }
+}
