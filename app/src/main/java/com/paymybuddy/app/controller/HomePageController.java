ec0a8b72411e8dc366c892389c8519a648b088c5
Quentin Caracatzanis <quentinversaille@gmail.com>
2022-12-27 17:47:11 +0100

    Added methods from AccountTransfer to HomePageController

--- app/src/main/java/com/paymybuddy/app/controller/HomePageController.java
+++ app/src/main/java/com/paymybuddy/app/controller/HomePageController.java
@@ -1,29 +1,55 @@
 package com.paymybuddy.app.controller;
 
+import com.paymybuddy.app.model.AccountTransfer;
 import com.paymybuddy.app.model.User;
-import com.paymybuddy.app.service.MoneyTransferService;
+import com.paymybuddy.app.service.AccountTransferService;
 import com.paymybuddy.app.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.math.BigDecimal;
 import java.security.Principal;
+import java.util.List;
 
 @Controller
+@RequestMapping("/home")
 public class HomePageController {
 
     @Autowired
     private UserService userService;
-
     @Autowired
-    private MoneyTransferService moneyTransferService;
+    private AccountTransferService accountTransferService;
 
-    @GetMapping("/home")
+
+    @GetMapping
     public String home(Principal principal, Model model) {
         User connectedUser = userService.findByEmail(principal.getName());
         model.addAttribute("firstName", connectedUser.getFirstName());
         model.addAttribute("balance", connectedUser.getCredit());
         return "home";
     }
+    @GetMapping("/accountTransfer")
+    public String getTransfers(Principal principal, Model model){
+        List<AccountTransfer> accountTransferList = accountTransferService.getAccountTransfers(principal);
+        model.addAttribute("transfers",accountTransferList);
+        return "bankTransfer";
+    }
+    @PostMapping("/transferToBank")
+    public String addTransferToBank (Principal principal, @RequestParam(value = "amount") BigDecimal Amount,
+                                     @RequestParam(value = "bankId") Long bankInfoId){
+        accountTransferService.addAccountTransfer(principal, Amount, bankInfoId, false);
+        return "redirect:/home";
+    }
+    @PostMapping("/transferFromBank")
+    public String addTransferFromBank (Principal principal,@RequestParam(value = "amount") BigDecimal Amount,
+                                       @RequestParam(value = "bankId") Long bankInfoId){
+        accountTransferService.addAccountTransfer(principal, Amount, bankInfoId, true);
+        return "redirect:/home";
+    }
+
 }
